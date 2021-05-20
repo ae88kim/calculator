0ebad0dcff293ec335de95b4e2e6ee2ac0d8beb8
ae88kim <ae88kim@gmail.com>
2021-05-20 17:37:24 +0900

    minus result fix

--- src/Backtoroman.java
+++ src/Backtoroman.java
@@ -1,23 +1,33 @@
 public class Backtoroman {
 
-        static String intToRoman(int num)
-        {
 
-            String c[] = {"", "C", "CC", "CCC", "CD", "D",
-                    "DC", "DCC", "DCCC", "CM"};
-            String x[] = {"", "X", "XX", "XXX", "XL", "L",
-                    "LX", "LXX", "LXXX", "XC"};
-            String i[] = {"", "I", "II", "III", "IV", "V",
-                    "VI", "VII", "VIII", "IX"};
+    static String intToRoman(int num)
+    {
+        boolean minus = false;
 
-            String hundereds = c[(num%1000)/100];
-            String tens = x[(num%100)/10];
-            String ones = i[num%10];
-
-            String answer = hundereds + tens + ones;
-
-            return answer;
+        if (num < 0) {
+            minus = true;
+            num = Math.abs(num);
         }
+
+        String c[] = {"", "C", "CC", "CCC", "CD", "D",
+                "DC", "DCC", "DCCC", "CM"};
+        String x[] = {"", "X", "XX", "XXX", "XL", "L",
+                "LX", "LXX", "LXXX", "XC"};
+        String i[] = {"", "I", "II", "III", "IV", "V",
+                "VI", "VII", "VIII", "IX"};
+
+        String hundereds = c[(num%1000)/100];
+        String tens = x[(num%100)/10];
+        String ones = i[num%10];
+
+        String answer = hundereds + tens + ones;
+        if (minus) {
+            answer = ("-"+answer);
+        }
+
+        return answer;
+    }
 
 
 }
