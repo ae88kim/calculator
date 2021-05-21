e070bea8ff12ba5aaa7958284a5c3065070d0b66
ae88kim <ae88kim@gmail.com>
2021-05-21 09:18:50 +0900

    int instead of double

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
         Input task = new Input();
         task.read();
         boolean res = task.getRomatrue();
-        double result = Calculator.calculate(task.getVar1(), task.getVar2(), task.getOper());
+        int result = Calculator.calculate(task.getVar1(), task.getVar2(), task.getOper());
 
             if (res) {
-                System.out.println("Output:" + "\n" + Backtoroman.intToRoman((int) result));
+                System.out.println("Output:" + "\n" + Backtoroman.intToRoman(result));
             }
             else {
-                System.out.println("Output:" + "\n" + (int) result);
+                System.out.println("Output:" + "\n" + result);
             }
             Start();
 
